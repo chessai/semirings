d12266da3a093203e417305656744d8dafb772f9
chessai <chessai1996@gmail.com>
2018-03-22 15:06:26 -0400

    Poly now has an Array backend instead of Vector

--- Data/Semiring/Poly.hs
+++ Data/Semiring/Poly.hs
@@ -4,9 +4,10 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
-{-# OPTIONS_GHC -Wall #-}
+--{-# OPTIONS_GHC -Wall #-}
 
 module Data.Semiring.Poly
   ( Poly(..)
@@ -16,96 +17,81 @@ module Data.Semiring.Poly
   , collapse
   , composePoly
   , horner
-  , vectorTimes
   ) where
 
 import           Control.Applicative (Applicative(liftA2))
 import           Data.Foldable (Foldable)
 import qualified Data.Foldable as Foldable
 --import           Data.Maybe
-import           Data.Vector (Vector)
-import qualified Data.Vector as Vector
+import           Data.Monoid (mempty)
+import           Data.Primitive.Array (Array)
+import qualified Data.Primitive.Array as Array
 import           GHC.Generics (Generic, Generic1)
 import qualified Prelude as P
 import           Prelude (($), (.), id)
 
+import Data.Ring (Ring(..), (-), minus)
 import Data.Semiring (Semiring(zero,one,plus,times), (+), (*))
 
 polyJoin :: Semiring a => Poly (Poly a) -> Poly a
 polyJoin (Poly x) = collapse x
 
-collapse :: Semiring a => Vector (Poly a) -> Poly a
+collapse :: Semiring a => Array (Poly a) -> Poly a
 collapse = Foldable.foldr composePoly zero
 
 -- | The type of polynomials in one variable
-newtype Poly  a   = Poly  (Vector a)
+newtype Poly  a   = Poly  (Array a)
   deriving (P.Eq, P.Ord, P.Read, P.Show, Generic, Generic1,
             P.Functor, P.Foldable)
 
 -- | The type of polynomials in two variables
-newtype Poly2 a b = Poly2 (Vector (a,b))
+newtype Poly2 a b = Poly2 (Array (a,b))
   deriving (P.Eq, P.Ord, P.Read, P.Show, Generic, Generic1,
             P.Functor)
 
 -- | The type of polynomials in three variables
-newtype Poly3 a b c = Poly3 (Vector (a,b,c))
+newtype Poly3 a b c = Poly3 (Array (a,b,c))
   deriving (P.Eq, P.Ord, P.Read, P.Show, Generic, Generic1,
             P.Functor)
 
-instance Applicative Poly where
-  pure = Poly . Vector.singleton
-  Poly f <*> Poly v = Poly $ Vector.zipWith id f v
-
 composePoly :: Semiring a => Poly a -> Poly a -> Poly a
-composePoly (Poly x) y = horner y (P.fmap (Poly . Vector.singleton) x)
+composePoly (Poly x) y = horner y (P.fmap (Poly . arraySingleton) x)
 
---instance P.Monad Poly where
---  p >>= f = polyJoin (P.fmap f p)
+arraySingleton :: a -> Array a
+arraySingleton = P.pure
+
+infixl 1 `bind`
+
+bind :: forall a b. Semiring b => Poly a -> (a -> Poly b) -> Poly b
+p `bind` f = polyJoin (P.fmap f p)
 
 -- | Horner's scheme for evaluating a polynomial in a semiring
 horner :: (Semiring a, Foldable t) => a -> t a -> a
 horner x = Foldable.foldr (\c val -> c + x * val) zero
 
-vectorPlus :: Semiring a => Vector a -> Vector a -> Vector a
-vectorPlus x y
-  = if Vector.null x then y else if Vector.null y then x else
-  liftA2 plus x y
+arrayPlus :: Semiring a => Array a -> Array a -> Array a
+arrayPlus x y = x
 
-vectorTimes :: Semiring a => Vector a -> Vector a -> Vector a
-vectorTimes x y
-  = if Vector.null x then x else if Vector.null y then y else
-  liftA2 times x y
-
--- | There is no way to do better than /O(n^2)/ for any
--- semiring. There is an algorithm that is /O(n log n)/,
--- but it requires something at least as strong as 'RealFloat'.
-polyTimes :: Semiring a => Vector a -> Vector a -> Vector a
-polyTimes x y
-  = if Vector.null x then x else if Vector.null y then y else
-      Vector.cons (times a b) (Vector.map (a *) q + Vector.map (* b) p + (Vector.cons zero (polyTimes p q)))
-  where
-    a = Vector.unsafeHead x
-    b = Vector.unsafeHead y
-    p = (\t d -> if Vector.null t then d else t) (Vector.tail x) (Vector.empty)
-    q = (\t d -> if Vector.null t then d else t) (Vector.tail y) (Vector.empty)
+polyTimes :: Semiring a => Array a -> Array a -> Array a
+polyTimes x y = x
 
 instance Semiring a => Semiring (Poly a) where
-  zero = Poly (Vector.empty)
-  one  = Poly (Vector.singleton one)
+  zero = Poly mempty
+  one  = Poly $ arraySingleton one
 
-  plus  (Poly x) (Poly y) = Poly $ vectorPlus  x y
+  plus  (Poly x) (Poly y) = Poly $ arrayPlus  x y
   times (Poly x) (Poly y) = Poly $ polyTimes x y
 
 instance (Semiring a, Semiring b) => Semiring (Poly2 a b) where
-  zero = Poly2 (Vector.empty)
-  one  = Poly2 (Vector.singleton one)
+  zero = Poly2 mempty
+  one  = Poly2 $ arraySingleton one
 
-  plus  (Poly2 x) (Poly2 y) = Poly2 $ vectorPlus  x y
+  plus  (Poly2 x) (Poly2 y) = Poly2 $ arrayPlus  x y
   times (Poly2 x) (Poly2 y) = Poly2 $ polyTimes x y
 
 instance (Semiring a, Semiring b, Semiring c) => Semiring (Poly3 a b c) where
-  zero = Poly3 (Vector.empty)
-  one  = Poly3 (Vector.singleton one)
+  zero = Poly3 mempty
+  one  = Poly3 $ arraySingleton one
 
-  plus  (Poly3 x) (Poly3 y) = Poly3 $ vectorPlus  x y
+  plus  (Poly3 x) (Poly3 y) = Poly3 $ arrayPlus  x y
   times (Poly3 x) (Poly3 y) = Poly3 $ polyTimes x y
