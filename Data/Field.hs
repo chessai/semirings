d0e189712acd1b49fa7bb455f1fd4e264f3ded26
Multramate <dka316@ic.ac.uk>
2019-08-20 05:14:15 +0100

    Remove redundant Num and Integral constraints

--- Data/Field.hs
+++ Data/Field.hs
@@ -4,10 +4,10 @@ module Data.Field
   , fromRational
   ) where
 
-import Prelude hiding (fromRational, quot, (/))
+import Prelude hiding (fromRational, (/))
 import Data.Complex (Complex)
-import Data.Euclidean (Euclidean, WrappedFractional, quot)
-import Data.Semiring (Ring, one)
+import qualified Data.Euclidean as R (Euclidean, WrappedFractional, quot)
+import qualified Data.Semiring as R (Ring, fromNatural, negate, one)
 import Foreign.C.Types (CDouble, CFloat)
 import GHC.Real (Ratio((:%)))
 
@@ -16,14 +16,14 @@ import GHC.Real (Ratio((:%)))
 ---------------------------------------------------------------------
 
 -- | A field is a ring with a multiplicative inverse for any non-zero element.
-class (Euclidean a, Ring a) => Field a where
+class (R.Euclidean a, R.Ring a) => Field a where
 
   -- | Divide two elements of a 'Field'.
   -- For any 'Prelude.Fractional' type, this is the same as '(Prelude./)'.
   --
   --     @x `divide` y = x `times` 'recip' y@
   divide :: a -> a -> a
-  divide = quot
+  divide = R.quot
   {-# INLINE divide #-}
 
   -- | Invert an element of a 'Field'.
@@ -31,20 +31,26 @@ class (Euclidean a, Ring a) => Field a where
   --
   --     @'recip' x `times` x = 'one'@
   recip :: a -> a
-  recip = quot one
+  recip = R.quot R.one
   {-# INLINE recip #-}
 
   -- | Infix shorthand for 'divide'.
   (/) :: a -> a -> a
-  (/) = quot
+  (/) = R.quot
   {-# INLINE (/) #-}
 
 infixl 7 `divide`
 infixl 7 /
 
 -- | Convert from rational to field.
-fromRational :: (Integral a, Field a, Num a) => Rational -> a
-fromRational (x :% y) = quot (fromIntegral x) (fromIntegral y)
+fromRational :: Field a => Rational -> a
+fromRational (x :% y)
+  | x >= 0 && y >= 0 = R.quot (fromN x) (fromN y)
+  | x <  0 && y >= 0 = R.quot (R.negate (fromN (negate x))) (fromN y)
+  | x >= 0 && y <  0 = R.quot (R.negate (fromN x)) (fromN (negate y))
+  | otherwise        = R.quot (fromN (negate x)) (fromN (negate y))
+  where
+    fromN = R.fromNatural . fromInteger
 {-# INLINE fromRational #-}
 
 ---------------------------------------------------------------------
@@ -63,6 +69,6 @@ instance Field CDouble
 
 instance Integral a => Field (Ratio a)
 
-instance (Eq a, Fractional a) => Field (WrappedFractional a)
+instance (Eq a, Fractional a) => Field (R.WrappedFractional a)
 
-instance (Eq a, Fractional a, Ring a) => Field (Complex a)
+instance (Eq a, Fractional a, R.Ring a) => Field (Complex a)
